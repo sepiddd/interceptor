5f4db679d509aff4202faeefda4053ee760c0f2f
sepiddd <sepide.khadempir@gmail.com>
2021-07-21 15:24:00 +0430

    add debounce function

--- src/services/interceptor.ts
+++ src/services/interceptor.ts
@@ -1,3 +1,15 @@
+let timeout;
+
+/**
+ * This function prevent from too much API calls
+ * @param {*} func is the input function
+ * @param {*} delay is the delay time in ms for recall the function
+ */
+function debounce(func, delay) {
+  clearTimeout(timeout);
+  timeout = setTimeout(func, delay);
+}
+
 function batchInterceptor(instance: any) {
   instance.interceptors.request.use(
     (request: any) => {
